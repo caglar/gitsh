60e56397c814c69b86f1633a4cbef376321829df
Squeeks <privacymyass@gmail.com>
2011-01-24 14:41:20 +0000

    Huge fix to installer - we now download latest from git with curl (and fail gracefully), write to /usr/local/bin/, and install Term::Shell.

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-perl -MCPAN -e "install Term::Shell"
+curl -s -o "/usr/local/bin/gitsh" https://github.com/caglar/gitsh/raw/master/gitsh.pl
 
-chmod +x /usr/local/bin/gitsh
-cp ./gitsh.pl /usr/bin/gitsh
+if [ $? -ne 0 ] ; then
+	echo "Error occurred getting URL /usr/local/bin/gitsh:"
+	if [ $? -eq 6 ]; then
+		echo "Unable to resolve host\n"
+	fi
+	if [$? -eq 7 ]; then
+		echo "Unable to connect to host\n"
+	fi
+	exit 1
+fi
+
+if [ -f "/usr/local/bin/gitsh" ] ; then
+	chmod +x /usr/local/bin/gitsh
+fi
+
+perl -MTerm::Shell -e 1 &> /dev/null
+
+if [ $? -ne 0 ] ; then
+	echo "Perl Module Term::Shell is not installed!"
+	perl -MCPAN -e "install Term::Shell"
+fi
